94851e65180d9e79420000f5e180f60f52bdfefc
mamadou s leigh <mamadousleigh@gmail.com>
2026-09-14 00:27:25 -1200

    commit 2

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Recipes = ({ recipes, onViewRecipes, selectRecipe }) => {
   return (
-    <div>
+    <div className="py-5 px-6">
       {selectRecipe ? (
-        <div className="max-w-2xl mx-auto mt-10 mb-10 bg-white rounded-2xl shadow-xl overflow-hidden px-5 py-7">
+        <div className="max-w-2xl mx-auto mt-10 mb-10 bg-white rounded-2xl shadow-xl overflow-hidden ">
           <img
             src={selectRecipe.strMealThumb}
             alt={selectRecipe.strMeal}
