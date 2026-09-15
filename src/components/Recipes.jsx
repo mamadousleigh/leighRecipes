12b4a1fc5d25607b20975df59a5be5e8eb1cf5a5
mamadou s leigh <mamadousleigh@gmail.com>
2026-09-15 00:59:58 -1200

    padding

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Recipes = ({ recipes, onViewRecipes, selectRecipe }) => {
   return (
-    <div className="py-5 px-6 mx-5 my-3 flex flex-col items-center justify-center ">
+    <div className="py-2 px-4  my-3 flex flex-col items-center justify-center ">
       {selectRecipe ? (
         <div className="w-full max-w-2xl mx-auto mt-10 mb-10 bg-white rounded-2xl shadow-xl overflow-hidden">
           <img
